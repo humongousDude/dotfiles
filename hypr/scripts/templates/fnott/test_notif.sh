47bbdffd5dab2e2c7d02cb96933d2be444126950
HumongousDude <youssefmelad1986@gmail.com>
2025-05-02 12:55:52 +0300

    some stuff idfk

---
+++ hypr/scripts/templates/fnott/test_notif.sh
@@ -0,0 +1,3 @@
+notify-send -a "MyApplicationName" "This is the low level summary" "Hello, World!" -u low
+notify-send -a "MyApplicationName" "This is the normal level summary" "Hello, World!" -u normal
+notify-send -a "MyApplicationName" "This is the critical level summary" "Hello, World!" -u critical
